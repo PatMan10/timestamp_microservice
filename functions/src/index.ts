33cb781e0e15fe56abf202b1f34ec09d02a602c1
PatMan10 <PatMan10@protonmail.com>
2020-03-15 20:25:30 +0200

    Done

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,29 @@
-import * as functions from 'firebase-functions';
+import functions = require("firebase-functions");
+import express = require("express");
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const app = express();
+
+app.get("/", (_req, res) => {
+  res.send("Hello world, this is Patman10!");
+});
+
+app.get("/api/timestamp/:date_string?", (req, res) => {
+  const date_string = req.params.date_string;
+  let date_utc, date_unix;
+  if (/^\d+$/.test(date_string)) date_unix = Number(date_string);
+  else date_utc = date_string;
+
+  let timestamp;
+  if (date_unix) timestamp = new Date(date_unix);
+  else if (date_utc) timestamp = new Date(date_utc);
+  else timestamp = new Date();
+
+  let jsonObj;
+  if (timestamp.toUTCString() === "Invalid Date")
+    jsonObj = { error: timestamp.toUTCString() };
+  else jsonObj = { unix: timestamp.getTime(), utc: timestamp.toUTCString() };
+
+  res.send(jsonObj);
+});
+
+export const App = functions.https.onRequest(app);
